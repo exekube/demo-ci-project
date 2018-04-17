839fb8547686667dbca02f43c15fcae839f41379
Ilya Sotkov <ilya@sotkov.com>
2018-04-17 14:59:14 +0300

    Add oauth_scope for chartmuseum, default zones / regions to modules

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -10,5 +10,14 @@ module "gke_cluster" {
   project_id         = "${var.project_id}"
   serviceaccount_key = "${var.serviceaccount_key}"
 
+  main_compute_zone = "europe-west1-d"
+
+  oauth_scopes = [
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/devstorage.read_write",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring",
+  ]
+
   initial_node_count = 2
 }
--- modules/gke-network/main.tf
+++ modules/gke-network/main.tf
@@ -19,6 +19,10 @@ module "gke_network" {
   project_id         = "${var.project_id}"
   serviceaccount_key = "${var.serviceaccount_key}"
 
+  cluster_subnets = {
+    "0" = "europe-west1,10.16.0.0/20,10.17.0.0/16,10.18.0.0/16"
+  }
+
   dns_zones   = "${var.dns_zones}"
   dns_records = "${var.dns_records}"
 }
